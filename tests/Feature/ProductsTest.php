0c8adf5ab11efba41896446604c734028fd7ffc7
LawrenceWebon <lawrencewebon@gmail.com>
2024-06-10 19:20:30 +0800

    Using Factories for Fake Testing Data

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 use function Pest\Laravel\get;
 
@@ -22,7 +23,19 @@ it('homepage contains non empty table', function () {
         ->assertDontSee(__('No products found'))
         ->assertSee($product->name)
         ->assertSee($product->price)
-        ->assertViewHas('products', function (Collection $collection) use ($product) {
+        // ->assertViewHas('products', function (Collection $collection) use ($product) {
+        ->assertViewHas('products', function (LengthAwarePaginator $collection) use ($product) { 
             return $collection->contains($product);
         }); 
 }); 
+
+it('paginated products table doesnt contain 11th record', function () { 
+    $products = Product::factory(11)->create(); 
+    $lastProduct = $products->last();
+ 
+    get('/products')
+        ->assertStatus(200)
+        ->assertViewHas('products', function (LengthAwarePaginator $collection) use ($lastProduct) {
+            return $collection->doesntContain($lastProduct);
+        });
+});
