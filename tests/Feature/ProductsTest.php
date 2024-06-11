52f1f98a662a96d22aaceb1ade6f6aaf6ebbb2dd
LawrenceWebon <lawrencewebon@gmail.com>
2024-06-11 11:34:00 +0800

    Refactor: Repeating Test Code into Hooks or setUp

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -8,24 +8,24 @@ use Illuminate\Pagination\LengthAwarePaginator;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
-it('homepage contains empty table', function () {
-    $user = User::factory()->create();
+beforeEach(function () {
+    $this->user = User::factory()->create();
+});
 
-    actingAs($user)
+it('homepage contains empty table', function () {
+    actingAs($this->user)
         ->get('/products')
         ->assertStatus(200)
         ->assertSee(__('No products found'));
 });
 
 it('homepage contains non empty table', function () {  
-    $user = User::factory()->create();
-
     $product = Product::create([
         'name'  => 'Product 1',
         'price' => 123,
     ]);
 
-    actingAs($user)
+    actingAs($this->user)
         ->get('/products')
         ->assertStatus(200)
         ->assertDontSee(__('No products found'))
@@ -37,11 +37,10 @@ it('homepage contains non empty table', function () {
 }); 
 
 it('paginated products table doesnt contain 11th record', function () { 
-    $user = User::factory()->create();
     $products = Product::factory(11)->create(); 
     $lastProduct = $products->last();
  
-    actingAs($user)
+    actingAs($this->user)
         ->get('/products')
         ->assertStatus(200)
         ->assertViewHas('products', function (LengthAwarePaginator $collection) use ($lastProduct) {
