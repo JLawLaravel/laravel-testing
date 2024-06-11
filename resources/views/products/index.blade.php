582e4973d31093399ea547c664591e6efbbbcb26
LawrenceWebon <lawrencewebon@gmail.com>
2024-06-11 09:17:27 +0800

    Auth Test: Is User Logged In?

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,47 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Products') }}
+        </h2>
+    </x-slot>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
-        <title>Document</title>
-    </head>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#ID</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">PRICE (USD)</th>
+                                <th scope="col">PRICE (EUR)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-    <body>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">PRICE (USD)</th>
-                    <th scope="col">PRICE (EUR)</th>
-                </tr>
-            </thead>
-            <tbody>
+                            @forelse ($products as $product)
+                                <tr>
+                                    <th>{{ $product->id }}</th>
+                                    <th>{{ $product->name }}</th>
+                                    <th>{{ $product->price }} USD</th>
+                                    <th>{{ $product->price_eur }} EUR</th>
+                                </tr>
+                            @empty
+                                <tr>{{ __('No products found') }}</tr>
+                            @endforelse
 
-                @forelse ($products as $product)
-                    <tr>
-                        <th>{{ $product->id }}</th>
-                        <th>{{ $product->name }}</th>
-                        <th>{{ $product->price }} USD</th>
-                        <th>{{ $product->price_eur }} EUR</th>
-                    </tr>
-                @empty
-                    <tr>{{ __('No products found') }}</tr>
-                @endforelse
-
-            </tbody>
-        </table>
-        {{ $products->links() }}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
-    </body>
-
-</html>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -1,39 +1,48 @@
 <?php
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
 it('homepage contains empty table', function () {
-    get('/products')
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->get('/products')
         ->assertStatus(200)
         ->assertSee(__('No products found'));
 });
 
 it('homepage contains non empty table', function () {  
+    $user = User::factory()->create();
+
     $product = Product::create([
         'name'  => 'Product 1',
         'price' => 123,
     ]);
 
-    get('/products')
+    actingAs($user)
+        ->get('/products')
         ->assertStatus(200)
         ->assertDontSee(__('No products found'))
         ->assertSee($product->name)
         ->assertSee($product->price)
-        // ->assertViewHas('products', function (Collection $collection) use ($product) {
         ->assertViewHas('products', function (LengthAwarePaginator $collection) use ($product) { 
             return $collection->contains($product);
         }); 
 }); 
 
 it('paginated products table doesnt contain 11th record', function () { 
+    $user = User::factory()->create();
     $products = Product::factory(11)->create(); 
     $lastProduct = $products->last();
  
-    get('/products')
+    actingAs($user)
+        ->get('/products')
         ->assertStatus(200)
         ->assertViewHas('products', function (LengthAwarePaginator $collection) use ($lastProduct) {
             return $collection->doesntContain($lastProduct);
